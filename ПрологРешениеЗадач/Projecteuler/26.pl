6a3027f70f19b597f8d227156b743d74f3e8c90e
Ilonica <95592035+Ilonica@users.noreply.github.com>
2024-06-08 04:49:15 +0300

    Create 26.pl

---
+++ ПрологРешениеЗадач/Projecteuler/26.pl
@@ -0,0 +1,32 @@
+Найдите число d, меньшее 1000, для которого десятичная дробь 1/d содержит самый 
+длинный период
+Задача должна быть решена без использования списков.
+
+
+#!/usr/bin/env swipl -Oqg main -s
+
+rlen(I, A, L) :-
+    atom_length(A, Len),
+    I = Len -> L is 1;
+    (
+        IB is I + 2,
+        sub_atom(A, 2, I, _, AA),
+        sub_atom(A, IB, I, _, AB),
+        AA = AB -> L is I + 1;
+        (I1 is I + 1, rlen(I1, A, L))
+    ).
+
+rlen(D, L) :-
+    N is 1 rdiv D,
+    format(atom(Ns), '~2000f', [N]),
+    rlen(2, Ns, L).
+
+rlen_bound(Start, End, Length) :-
+    between(Start, End, D),
+    rlen(D, Length).
+
+solve(X) :- aggregate(max(L), rlen_bound(1, 1000, L), X).
+
+main(_) :-
+    solve(X),
+    write(X), nl.
